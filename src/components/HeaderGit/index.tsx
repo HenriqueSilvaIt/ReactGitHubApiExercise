9413126863bf305a26a01dcc596e21ff253a4012
HenriqueSilvaIt <hikysilva2@gmail.com>
2025-02-26 15:13:28 -0300

    Add nav link route

--- src/components/HeaderGit/index.tsx
+++ src/components/HeaderGit/index.tsx
@@ -1,12 +1,17 @@
+import { Link } from 'react-router-dom';
 import './style.css';
 
 export default function HeaderGit() {
 
     return (
             <header>
+                
                 <nav className="git-container"> 
+                <Link to="/home" >
                     <h1>GitHub API</h1>
+                    </Link>
                 </nav>
+             
             </header>
     );
 }
