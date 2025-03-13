a18f413722714b7e392d1566afec6864bf7e1c8e
HenriqueSilvaIt <hikysilva2@gmail.com>
2025-03-13 19:07:01 -0300

    Adjust

--- src/routes/Home/GitSearch/index.tsx
+++ src/routes/Home/GitSearch/index.tsx
@@ -1,9 +1,4 @@
-
-import { useState } from 'react';
-import CardProfileGit from '../../../components/CardProfileGit';
-
 import CardSearchGit from '../../../components/CardSearchGit';
-import { ProfileDTO } from '../../../models/ProfileDTO';
 
 export default function GitSearch() {
 
